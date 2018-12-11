b0f2d97dd66059269bcc0607a52bc54c3d60fd50
hinamiqi <hinamiqi@gmail.com>
2018-12-11 23:03:38 +0300

    + buttons with event listener

--- MyFrame.java
+++ MyFrame.java
@@ -5,9 +5,13 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 class MyFrame extends JFrame {
 
   JLabel imgLabel;
+  // JButton captureBtn;
 
   // constructor
   MyFrame( String title, int wsize, int hsize ) {
@@ -15,10 +19,39 @@ class MyFrame extends JFrame {
     setSize( wsize, hsize );
     setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
     this.imgLabel = new JLabel();
+
+
     CreateMenuBar();
+    CreateButtonBar();
     pack(); //устанавливет размер jframe достаточный для расположения всех элементов
     setLocationRelativeTo(null);
     setResizable(false);
+
+  }
+
+  private void CreateButtonBar() {
+      JPanel buttonBar = new JPanel();
+      JButton captureBtn = new JButton("Capture");
+      JButton exitBtn = new JButton("Exit");
+      buttonBar.add(captureBtn);
+      buttonBar.add(exitBtn);
+
+      this.getContentPane().add(BorderLayout.SOUTH, buttonBar);
+      captureBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+					System.out.println("Capture button pressed");
+			}
+	      });
+      exitBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+					System.out.println("Exit button pressed");
+                    this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+			}
+	      });
   }
 
   private void CreateMenuBar() {
